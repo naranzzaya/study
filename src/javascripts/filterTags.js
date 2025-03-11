c36ce1b792aaa1c2b5509ecccb15cc2f44ab3134
Zay <z.znaranzaya@gmail.com>
2025-03-11 11:39:24 +0300

    js

--- src/javascripts/filterTags.js
+++ src/javascripts/filterTags.js
@@ -1,2 +1,21 @@
 console.clear()
 console.log('filterTags')
+document.addEventListener('DOMContentLoaded', () => {
+  initFilter()
+})
+
+function initFilter() {
+  const tags = document.querySelectorAll('.A_FilterTag')
+  const a = document.querySelector('.all')
+
+  tags.forEach((tag) => {
+    tag.addEventListener('click', () => {
+      if (tag != a) {
+        a.classList.remove('active')
+        tag.classList.toggle('active')
+
+        console.log('здесь будет функция фильтрации по ')
+      }
+    })
+  })
+}
